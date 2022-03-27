d066273ecea968a970b1f5a2a8998b5211e07bec
Diogo Centeno <78360687+sellingjaguar@users.noreply.github.com>
2022-03-27 02:29:43 +0100

    Add files via upload

---
+++ marcelopp.h
@@ -0,0 +1,76 @@
+#pragma once
+
+//https://www.educba.com/c-plus-plus-keywords/
+
+#define automatico auto
+#define duplo double
+#define inteiro int
+#define estrutura struct
+#define quebrar break
+#define partir break
+#define senao else
+#define longo long
+#define trocar switch
+#define caso case
+#define enumera enum
+#define registar register
+#define tipodeficiente typedef
+#define caracter char
+#define externo extern
+#define devolve return
+#define une union
+#define constante const
+#define flutuante float
+#define curto short
+#define semsinal unsigned
+#define prossegue continue
+#define para for
+#define comsinal signed
+#define vacuo void
+#define padrao default
+#define irpara goto
+#define tamanhode sizeof
+#define volatil volatile
+#define fazer do
+#define se if
+#define estatico static
+#define enquanto while
+#define asma asm
+#define feitico_dinamico dynamic_cast
+#define espaconome namespace
+#define feitico_reinterpretado reinterpret_cast
+#define booleano bool
+#define explicito explicit
+#define novo new
+#define feitico_estatico static_cast
+#define apanhar catch
+#define falso false
+#define operador operator
+#define modelo template
+#define classe class
+#define amigo friend
+#define privado private
+#define isto this
+#define feitico_constante const_cast
+#define entrelinha inline
+#define publico public
+#define atirar throw
+#define apagar delete
+#define mutavel mutable
+#define protegido protected
+#define verdadeiro true
+#define tentar try
+#define identificacaodetipo typeid
+#define nomedetipo typename
+#define utilizando using
+#define virtualizado virtual
+#define vitoriacaracter_t wchar_t
+
+#define cfora cout
+#define cdentro cin
+
+#define corda string
+
+#define fimdelinha endl
+
+#define principal main
